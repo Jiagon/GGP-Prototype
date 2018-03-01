f565d24e7a51e95dce1e1b3641d7fff307dd9717
sxl9302 <sxl9302@g.rit.edu>
2018-03-01 11:37:38 -0500

    Player movement complete

--- GGP Prototype/Assets/Scripts/PlayerMovement.cs
+++ GGP Prototype/Assets/Scripts/PlayerMovement.cs
@@ -7,74 +7,98 @@ public class PlayerMovement : MonoBehaviour {
     Camera cam;
     CursorLockMode cursor;
 
+    public float mouseSensitivity = 100.0f;
+    public float clampAngle = 80.0f;
+    public float maxSpeed = 10f;
+
+    float rotY = 0.0f;              // Rotation around the up / y axis - keyboard Q / E
+    float rotX = 0.0f;              // Rotation around the right / x axis - mouse movement
+    float speed = 4f;
+
     // Use this for initialization
     void Start() {
-        cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-        cursor = CursorLockMode.Confined;
+        cursor = CursorLockMode.Locked;
         Cursor.lockState = cursor;
+
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
     }
 
     // Update is called once per frame
     void Update() {
+        // Escapes program - more importantly escapes CursorLockMode.Locked so you can exit
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Application.Quit();
         }
-        float speed = 2;
+
+        // For player strafing / movement
         Vector3 pos = transform.position;
 
-        float sensitivity = 0.05f;
-        Vector3 vTV = cam.ViewportToScreenPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.nearClipPlane));
-        Vector3 vTS = cam.ViewportToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.nearClipPlane));
-        Vector3 vTW = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.nearClipPlane));
-        transform.LookAt(vTV, Vector3.up);
-        /*Vector3 vp = cam.ScreenToViewportPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.nearClipPlane));
-        vp.x -= 0.5f;
-        vp.y -= 0.5f;
-        vp.x *= sensitivity;
-        vp.y *= sensitivity;
-        vp.x += 0.5f;
-        vp.y += 0.5f;
+        // For camera movement
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = -Input.GetAxis("Mouse Y");
 
-        Vector3 sp = cam.ViewportToScreenPoint(vp);
+        // rotX = up and down camera
+        // rotY = left and right camera
+        rotX += mouseY * mouseSensitivity * Time.deltaTime;
+        // Clamping rotations
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+        
+        // For rotating and moving in the forward direction
+        Vector3 RIGHT = transform.TransformDirection(Vector3.right);
+        Vector3 LEFT = transform.TransformDirection(Vector3.left);
+        Vector3 FORWARD = transform.TransformDirection(Vector3.forward);
 
-        Vector3 v = cam.ScreenToWorldPoint(sp);
-        transform.LookAt(v, Vector3.up);*/
-
-        //transform.LookAt(cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.nearClipPlane)), Vector3.up);
-
+        // Speed up / slow down
         if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
         {
-            speed *= 2f;
+            speed += Time.deltaTime * 3;
+            speed = Mathf.Clamp(speed, 0, maxSpeed);
         }
         if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
         {
-            speed *= .5f;
+            speed -= Time.deltaTime * 3;
+            speed = Mathf.Clamp(speed, 0, maxSpeed);
         }
+
+        // Strafe up, down, left, right
         if (Input.GetKey(KeyCode.W))
         {
             pos.y += Time.deltaTime * speed;
+            transform.position = pos;
         }
         if (Input.GetKey(KeyCode.S))
         {
             pos.y -= Time.deltaTime * speed;
+            transform.position = pos;
         }
         if (Input.GetKey(KeyCode.A))
         {
-
+            transform.localPosition += RIGHT * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.D))
         {
-
+            transform.localPosition += LEFT * Input.GetAxis("Horizontal") * speed * Time.deltaTime * -1;
         }
+
+        // Rotate camera around Y axis
         if (Input.GetKey(KeyCode.Q))
         {
-
+            rotY -= Time.deltaTime * speed;
         }
         if (Input.GetKey(KeyCode.E))
         {
-
+            rotY += Time.deltaTime * speed;
         }
-        transform.position = pos;
-	}
+
+        // Moves player forward
+        transform.localPosition += FORWARD * speed * Time.deltaTime;
+
+        // Setting Rotation and forward vector
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+        transform.forward = transform.rotation * transform.forward;
+    }
 }
